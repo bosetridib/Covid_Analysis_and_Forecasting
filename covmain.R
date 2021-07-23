642a9d82e49d03e65eca7ce04d2fb204e506be15
bosetridib <bosetridib@yahoo.com>
2021-07-23 13:52:16 +0530

    ...

--- covmain.R
+++ covmain.R
@@ -21,18 +21,54 @@ daily_cases_df <- data.frame('date' = as.Date(case_dates) , 'cases' = daily_case
 # Let us check the plot of the data in original and in differenced form.
 plot(daily_cases_df, type = "l")
 
-# In the differenced form of the data, the increasing variance in the rate of
-# change of new covid cases can be seen.
-# plot(
-#   daily_cases_df$date[2:length(daily_cases)],
-#   diff(daily_cases_df$cases),
-#   type = "l"
-# )
-
-# There are supposedly three waves. Hence, the two visible waves can be deduced
+# There are supposedly THREE WAVES. Hence, the two visible waves can be deduced
 # into log-normal and normal distribution. The third wave can be assumed to be
 # symmetrically log-normal like the first one.
 
-# The deconstruction can be done as below.
+# ================= The deconstruction can be done as below ================= #
 
-plot(daily_cases_df[daily_cases_df$date < "2021-03-01",], type = "l")
+# Under visual sight, the first wave ends somewhere in Feb-Apr.
+plot(daily_cases_df[daily_cases_df$date > "2021-01-01",], type = "l")
+
+# Indexing on those months, we set
+ind <- ("2021-02-01" < daily_cases_df$date) & (daily_cases_df$date < "2021-04-01")
+# And we get the first wave end date as
+first_end <- daily_cases_df$date[ind][
+  daily_cases_df$cases[ind] == min(daily_cases_df$cases[ind])
+]
+
+# Similarly, the second wave ending can also be obtained.
+plot(daily_cases_df[daily_cases_df$date > "2021-05-01",], type = "l")
+
+# Seems the second ends between June.
+ind <- ("2021-06-01" < daily_cases_df$date) & (daily_cases_df$date < "2021-06-30")
+
+second_end <- daily_cases_df$date[ind][
+  daily_cases_df$cases[ind] == min(daily_cases_df$cases[ind])
+]
+
+rm(ind)
+# Now, we split the data on the basis of first, second and upcoming third wave.
+first_wave_df <- daily_cases_df[daily_cases_df$date <= first_end,]
+
+second_wave_df <- daily_cases_df[
+  (daily_cases_df$date > first_end) & (daily_cases_df$date <= second_end),
+]
+
+third_wave_df <- daily_cases_df[daily_cases_df$date > second_end,]
+
+# ----------------- The first wave log-normal trend ----------------- #
+
+# The function that returns the residual sum squared for given parameters
+x <- length(first_wave_df$cases)
+residual_sum_squared <- function(parameter) {
+  return(
+    sum(
+      ( first_wave_df$cases - 
+          (parameter[1]*dlnorm(
+            x, mean = parameter[2], sd = parameter[3]
+          ))
+        )^2
+    )
+  )
+}
